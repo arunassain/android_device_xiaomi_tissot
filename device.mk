0f7162d9c94030578e3b3b1f52f54e16ebc2ef1c
TheScarastic <warabhishek@gmail.com>
2018-03-22 23:37:14 +0530

    tissot: Import xiaomidoze from xiaomi/msm8996

--- device.mk
+++ device.mk
@@ -183,6 +183,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
