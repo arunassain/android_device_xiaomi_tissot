e1fbdd57c7bbe146a67dc3dc471c110bcad5f862
VenkatVishalV <venkatvishal124@gmail.com>
2018-04-12 09:29:43 +0000

    Tissot: use proper power packages for AOSP-CAF
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- device.mk
+++ device.mk
@@ -323,8 +323,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-impl \
-    android.hardware.power@1.1-service-qti
+    power.msm8953 \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # QMI
 PRODUCT_PACKAGES += \
