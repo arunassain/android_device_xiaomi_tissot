80e6231fae1c1a62c85aca04a61319b956871500
arunassain <arun.assain98@gmail.com>
2018-05-08 12:34:41 +0000

    omni-ify

--- omni_tissot.mk
+++ omni_tissot.mk
@@ -17,7 +17,8 @@
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -52,7 +53,7 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libgptutils \
     libz
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := omni_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
