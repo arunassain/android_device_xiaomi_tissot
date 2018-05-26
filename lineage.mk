d85d2c58f79adcd9afbcb3e11fa3fe5187f6e06e
arunassain <arun.assain98@gmail.com>
2018-05-26 05:59:33 +0000

    Revert "tissot: Update build fingerprint"
    This reverts commit a2ad5f409172067b32600446f64f8d29d3c8434c.

--- lineage.mk
+++ lineage.mk
@@ -58,6 +58,6 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.1.0 OPM1.171019.019 8.4.28 release-keys"
-
-BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:OPM1.171019.019/8.4.28:user/release-keys
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    
+BUILD_FINGERPRINT="xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
