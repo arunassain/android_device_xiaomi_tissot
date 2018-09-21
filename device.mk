522dc9fc574d614a66194259f033d63522d726b5
VenkatVishalV <venkatvishal124@gmail.com>
2018-09-21 10:53:04 +0000

    tissot: inherit msm8953-common vendor

--- device.mk
+++ device.mk
@@ -16,7 +16,8 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/xiaomi/tissot/tissot-vendor.mk)
+$(call inherit-product, vendor/xiaomi/tissot/tissot-vendor.mk) 
+$(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
